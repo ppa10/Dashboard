60b698b33f97a15499a1b26832480feb0da91c7e
alberteetac <albertlillo8@gmail.com>
2019-05-26 22:39:10 +0200

    ParteGruposVisual

--- src/app/paginas/editar-grupo/editar-grupo.component.ts
+++ src/app/paginas/editar-grupo/editar-grupo.component.ts
@@ -149,7 +149,7 @@ export class EditarGrupoComponent implements OnInit {
   // SI QUEREMOS AÑADIR ALUMNOS MANUALMENTE LO HAREMOS EN UN DIALOGO
   AbrirDialogoAgregarAlumnos(): void {
     const dialogRef = this.dialog.open(AgregarAlumnoDialogComponent, {
-      width: '750px',
+      width: '950px',
       // Le pasamos solo los id del grupo y profesor ya que es lo único que hace falta para vincular los alumnos
       data: {
         grupoId: this.grupoSeleccionado.id,
