7f9d460253470a3ef4e5e5e44ac60780001c41cd
alberteetac <albertlillo8@gmail.com>
2019-05-31 22:56:24 +0200

    Visual2

--- src/app/paginas/mis-puntos/mis-puntos.component.ts
+++ src/app/paginas/mis-puntos/mis-puntos.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { Location } from '@angular/common';
+
+// Servicios
+import { PuntosInsigniasService, ProfesorService } from '../../servicios/index';
+
+// Clases
+import { Punto, Insignia } from '../../clases/index';
+
 
 @Component({
   selector: 'app-mis-puntos',
@@ -7,9 +17,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisPuntosComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['nombre', 'descripcion', ' '];
+  profesorId: number;
+  listaPuntos: Punto[];
+
+// LO USAREMOS PARA EL ROUTE AL SIGUIENTE COMPONENTE
+returnUrl: string;
+
+// IDENTIFICADOR ÚNICO DEL PROFESOR QUE LO RECUPERAREMOS DE LA URL
+identificadorProfesor: number;
+
+  constructor(
+    private puntosInsigniasService: PuntosInsigniasService,
+    private profesorService: ProfesorService,
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  this.profesorId = Number (this.route.snapshot.paramMap.get('id'));
+
+  console.log(this.profesorId);
   }
 
 }
